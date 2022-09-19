f00419272e04a4bfc1f9c3ddec17aa357bab74c5
Jie <jie@xiechenruideMacBook-Air.local>
2022-09-19 22:44:29 +0800

    改成stays.php

--- product/stays/stays-del.php
+++ product/stays/stays-del.php
@@ -1,9 +1,10 @@
 <?php require __DIR__ . '/../../parts/connect_athome_db.php';
-$pageName = 'stays_insert';
+$pageName = 'stays_delete';
 ?>
+
 
 <?php include __DIR__ . '/../../parts/html-head.php'; ?>
 <?php include __DIR__ . '/../../parts/navbar.php'; ?>
-新增
+刪除
 <?php include __DIR__ . '/../../parts/script.php';?>
 <?php include __DIR__ . '/../../parts/html-foot.php'; ?>
