ac149662e0a021d409019206ca3f3f522fb71756
Huang-Jian-Rong <lvsonylg@gmail.com>
2022-09-19 17:14:55 +0800

    ticket list

--- product/ticket/ticket-list.php
+++ product/ticket/ticket-list.php
@@ -11,10 +11,10 @@ exit;
 
 ?>
 
-<?php require __DIR__ . '/parts/html-head.php'; ?>
+<?php require __DIR__ . '/../../parts/html-head.php'; ?>
 
 
-<?php include __DIR__ . '/parts/navbar.php'; ?>
+<?php include __DIR__ . '/../../parts/navbar.php'; ?>
 <div class="container">
 <div class="row">
         <div class="col">
@@ -56,6 +56,6 @@ exit;
     ?>
 
 
-<?php include __DIR__ . '/parts/scripts.php'; ?>
+<?php include __DIR__ . '/../../parts/script.php'; ?>
 
-<?php include __DIR__ . '/parts/html-foot.php'; ?>
+<?php include __DIR__ . '/../../parts/html-foot.php'; ?>
