9e43384cf8927e786b868fa99185f717d554d7e3
Liufat <dd5672003@gmail.com>
2022-09-19 16:58:24 +0800

    加入表單

--- cart/cart-list.php
+++ cart/cart-list.php
@@ -1,8 +1,89 @@
+<?php require __DIR__ . '/../parts/connect_db.php' ?>
 <?php include __DIR__ . '/../parts/html-head.php'; ?>
 <?php include __DIR__ . '/../parts/navbar.php'; ?>
 
 <div class="container">
     <h3 class="text-center">購物車</h3>
 </div>
+<div class="container">
+    <div class="row">
+        <table class="table">
+            <thead>
+                <h3>美食</h3>
+                <tr>
+                    <th scope="col">刪除</th>
+                    <th scope="col">編號</th>
+                    <th scope="col">預覽圖</th>
+                    <th scope="col">商品名稱</th>
+                    <th scope="col">單價</th>
+                    <th scope="col">小計</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
 
+            </tbody>
+        </table>
+
+        <table class="table">
+            <thead>
+                <h3>票券</h3>
+                <tr>
+                    <th scope="col">刪除</th>
+                    <th scope="col">編號</th>
+                    <th scope="col">商品名稱</th>
+                    <th scope="col">單價</th>
+                    <th scope="col">小計</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            </tbody>
+        </table>
+
+        <table class="table">
+            <thead>
+                <h3>住宿</h3>
+                <tr>
+                    <th scope="col">刪除</th>
+                    <th scope="col">編號</th>
+                    <th scope="col">預覽圖</th>
+                    <th scope="col">名稱</th>
+                    <th scope="col">房型</th>
+                    <th scope="col">總人數</th>
+                    <th scope="col">預計入住時間</th>
+                    <th scope="col">預計退房時間</th>
+                    <th scope="col">特殊需求</th>
+                    <th scope="col">小計</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php include __DIR__ . '/../parts/html-foot.php'; ?>
 <?php include __DIR__ . '/../parts/html-foot.php'; ?>
