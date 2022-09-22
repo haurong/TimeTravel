4699d9ec1bc687d78b7ad79c477028e6edcba22e
Liufat <dd5672003@gmail.com>
2022-09-22 15:40:02 +0800

    新增住宿與票券欄位

--- cart/cart-list.php
+++ cart/cart-list.php
@@ -1,18 +1,34 @@
 <?php require __DIR__ . '/../parts/connect_db.php' ?>
 <?php include __DIR__ . '/../parts/html-head.php'; ?>
 <?php include __DIR__ . '/../parts/navbar.php'; ?>
+<?php
+if (!isset($_SESSION['food-cart'])) {
+    $_SESSION['food-cart'] = [];
+}
+if (!isset($_SESSION['hotel-cart'])) {
+    $_SESSION['hotel-cart'] = [];
+}
+if (!isset($_SESSION['ticket-cart'])) {
+    $_SESSION['ticket-cart'] = [];
+}
+
+?>
 <?php $food = $_SESSION['food-cart']; ?>
+<?php $hotel = $_SESSION['hotel-cart'] ?>
+<?php $ticket = $_SESSION['ticket-cart'] ?>
 
 <div class="container">
     <h3 class="text-center">購物車</h3>
 </div>
 <div class="container">
     <div class="row">
-
         <table class="table">
             <thead>
                 <h3>美食</h3>
                 <tr>
+                    <th scope="col">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </th>
                     <th scope="col">商品名稱</th>
                     <th scope="col">商品圖片</th>
                     <th scope="col">適用商家</th>
@@ -26,17 +42,89 @@
                 <?php
                 foreach ($food as $k => $val) { ?>
                     <tr>
+                        <td>
+                            <a href="javascript: delete_it(<?= $r['sid'] ?>)">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
                         <td><?= $val['product_name'] ?></td>
                         <td><?= $val['product_photo'] ?></td>
                         <td><?= $val['p_selling_price'] ?></td>
                         <td><?= $val['applicable_store'] ?></td>
-                        <td><p><?= $val['qty'] ?></p></td>
+                        <td>
+                            <p><?= $val['qty'] ?></p>
+                        </td>
                         <td><?= $val['p_selling_price'] * $val['qty']; ?></td>
                     <?php } ?>
                     </tr>
             </tbody>
         </table>
-        </form>
+        <table class="table">
+            <thead>
+                <h3>住宿</h3>
+                <tr>
+                    <th scope="col">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </th>
+                    <th scope="col">飯店名稱</th>
+                    <th scope="col">飯店圖片</th>
+                    <th scope="col">飯店地址</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotel as $k => $val) { ?>
+                    <tr>
+                        <td>
+                            <a href="javascript: delete_it(<?= $r['sid'] ?>)">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                        <td><?= $val['hotel_name'] ?></td>
+                        <td><?= $val['picture'] ?></td>
+                        <td><?= $val['address'] ?></td>
+                    <?php } ?>
+                    </tr>
+            </tbody>
+        </table>
+        <table class="table">
+            <thead>
+                <h3>票券</h3>
+                <tr>
+                    <th scope="col">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </th>
+                    <th scope="col">商品名稱</th>
+                    <th scope="col">商品圖片</th>
+                    <th scope="col">介紹</th>
+                    <th scope="col">單價</th>
+                    <th scope="col">數量</th>
+                    <th scope="col">總價</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($ticket as $k => $val) { ?>
+                    <tr>
+                        <td>
+                            <a href="javascript: delete_it(<?= $r['sid'] ?>)">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                        <td><?= $val['product_name'] ?></td>
+                        <td><?= $val['product_imgs'] ?></td>
+                        <td><?= $val['product_introduction'] ?></td>
+                        <td><?= $val['product_price'] ?></td>
+                        <td>
+                            <p><?= $val['qty'] ?></p>
+                        </td>
+                        <td><?= $val['product_price'] * $val['qty']; ?></td>
+                    <?php } ?>
+                    </tr>
+            </tbody>
+        </table>
 
     </div>
 </div>
