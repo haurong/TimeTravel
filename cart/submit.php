30b3cde6ec66e4e74c24b787c1c410a0703389bd
Liufat <dd5672003@gmail.com>
2022-09-24 22:55:17 +0800

    修正空的SESSION傳入資料庫引起的錯誤

--- cart/submit.php
+++ cart/submit.php
@@ -10,6 +10,7 @@ require __DIR__ . './../parts/connect_db.php';
 //     exit;
 // }
 //
+
 $o_sql = "INSERT INTO `orders`(`member_sid`, `orders_created_time`) VALUES (?,  NOW())";
 $o_stmt = $pdo->prepare($o_sql);
 $o_stmt->execute([
@@ -18,31 +19,36 @@ $o_stmt->execute([
 // TODO: 檢查欄位資料
 
 $orders_sid = $pdo->lastInsertId(); // 最近新增資料的 PK
+if (!empty($_SESSION['food-cart'])) {
+    $odf_sql = "INSERT INTO `orders_details_food`(`orders_sid`, `food_products_sid`, `quantity`, `total_price`) VALUES (?, ?, ?, ?)";
+    $odf_stmt = $pdo->prepare($odf_sql);
+    $odf_stmt->execute([
+        $orders_sid,
+        $_POST['food_sid'],
+        $_POST['food_quantity'],
+        $_POST['food_total_price']
+    ]);
+}
+if (!empty($_SESSION['hotel-cart'])) {
+    $odh_sql = "INSERT INTO `orders_details_hotel`(`orders_sid`, `hotel_products_sid`) VALUES (?,?)";
+    $odh_stmt = $pdo->prepare($odh_sql);
+    $odh_stmt->execute([
+        $orders_sid,
+        $_POST['hotel_sid']
+    ]);
+}
+if (!empty($_SESSION['ticket-cart'])) {
+    $odt_sql = "INSERT INTO `orders_details_ticket`(`orders_sid`, `ticket_products_sid`,`quantity`,`total_price`) VALUES (?,?,?,?)";
+    $odt_stmt = $pdo->prepare($odt_sql);
+    $odt_stmt->execute([
+        $orders_sid,
+        $_POST['ticket_sid'],
+        $_POST['ticket_quantity'],
+        $_POST['ticket_total_price']
+    ]);
+}
 
-$odf_sql = "INSERT INTO `orders_details_food`(`orders_sid`, `food_products_sid`, `quantity`, `total_price`) VALUES (?, ?, ?, ?)";
-$odf_stmt = $pdo->prepare($odf_sql);
-$odf_stmt->execute([
-    $orders_sid,
-    $_POST['food_sid'],
-    $_POST['food_quantity'],
-    $_POST['food_total_price']
-]);
-
-$odh_sql = "INSERT INTO `orders_details_hotel`(`orders_sid`, `hotel_products_sid`) VALUES (?,?)";
-$odh_stmt = $pdo->prepare($odh_sql);
-$odh_stmt->execute([
-    $orders_sid,
-    $_POST['hotel_sid']
-]);			
-
-$odt_sql = "INSERT INTO `orders_details_ticket`(`orders_sid`, `ticket_products_sid`,`quantity`,`total_price`) VALUES (?,?,?,?)";
-$odt_stmt = $pdo->prepare($odt_sql);
-$odt_stmt->execute([
-    $orders_sid,
-    $_POST['ticket_sid'],
-    $_POST['ticket_quantity'],
-    $_POST['ticket_total_price']
-]);
+header('Location: ?page=' . $index);
 
 
 
